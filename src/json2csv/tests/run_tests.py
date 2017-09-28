84e72a1691919aaf228eb65b08cf84cdfc1988c9
Daniel Izquierdo Cortazar <dicortazar@gmail.com>
2017-09-28 11:18:22 +0200

    Update copyright date

--- src/json2csv/tests/run_tests.py
+++ src/json2csv/tests/run_tests.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2016 Bitergia
+# Copyright (C) 2017 Bitergia
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
