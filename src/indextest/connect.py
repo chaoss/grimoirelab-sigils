a6e07169e2244907956ab67b61514b3272e32bf4
Valerio Cosentino <valcos@bitergia.com>
2019-07-15 09:55:39 +0200

    [Sigils] Update license and copyright info
    This code updates the license info based on
    the content of https://www.gnu.org/licenses/gpl-3.0.html.
    Furthermore, it also extends the copyrigth timeframe
    to 2019.
    
    Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

--- src/indextest/connect.py
+++ src/indextest/connect.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015-2016 Bitergia
+# Copyright (C) 2015-2019 Bitergia
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,8 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 # Authors:
 #     Daniel Izquierdo Cortazar <dizquierdo@bitergia.com>
